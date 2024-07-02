62a7b4f755b48c33728b33a69fab62c63dc4885e
jabir756 <jabirkagone6@gmail>
2024-07-01 22:15:54 -0400

    mail systeme

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('contact').addEventListener('click', function() {
-        var email = "example@example.com";
-        var subject = "Sujet de l'email";
-        var body = "Contenu de l'email";
-        var mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+document.addEventListener('DOMContentLoaded',()=> {
+    document.getElementById('contact').addEventListener('click', ()=> {
+        let email = "example@example.com";
+        let subject = "Sujet de l'email";
+        let body = "Contenu de l'email";
+        let mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
         window.location.href = mailtoLink;
     });
 });
