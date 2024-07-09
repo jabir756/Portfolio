14768ad369d1ce0685461b5881d0683c5575972f
jabir756 <jabirkagone6@gmail>
2024-07-09 12:01:05 -0400

    remove script content

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    const projectLink = document.getElementById('projects');
-    const projectsContainer = document.getElementById('projects-container');
-
-    projectsContainer.style.display = 'none';
-
-    projectLink.addEventListener('click', ()=>{
-        projectsContainer.style.display = projectsContainer.style.display === 'none'? 'grid' : 'none';
-    });
-
 });
