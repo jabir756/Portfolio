ad6add73da919b28225dad62e8c641218dac9c05
jabir756 <jabirkagone6@gmail>
2024-07-18 17:13:47 -0400

    cv link

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 document.addEventListener('DOMContentLoaded',()=>{
+    document.getElementById('cv').addEventListener('click', ()=>{
+        window.open('https://drive.google.com/file/d/1QYOTuz_jI0riKeWBICb91fQ9KXApdr8o/view')
+    })
 });
