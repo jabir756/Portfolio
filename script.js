54d4d7c0838a0f905fb227888ff6bda133229feb
jabir756 <jabirkagone6@gmail>
2024-07-27 14:35:25 -0400

    styles

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Bouton de retour en haut de page
+    const backToTopButton = document.getElementById("back-to-top");
+    
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 200) {
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    });
+
+    backToTopButton.addEventListener("click", function() {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
